293a0c8126ccab45c0c524a2a6130d339be8b504
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2022-12-08 09:39:04 +0400

    Update aleo.py

--- aleo.py
+++ aleo.py
@@ -4,11 +4,14 @@ import time
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-API_TOKEN = "2109:AA-LL_EEOO"
-CHAT_ID = "-1703"
+
 COINBASE_MIN = 1
 INCENTIVES_MIN = 0.01
+SOLUTIONS_MIN = 2
 SLEEP_TIME_SEC = 360
+
+API_TOKEN = "2109:AA-LL_EEOO"
+CHAT_ID = "-1703"
 ADDRESSES = {
     "num1-serv1": "aleo...address1",
     "num2-serv2": "aleo...address2",
@@ -38,6 +41,11 @@ while True:
         else:
             soup = BeautifulSoup(page.content, 'html.parser')
             address_row = soup.findAll('div', {'class': 'address-second-table'})
+
+            solution_result = address_row[1].findAll('span', {'class': 'light-blue-number'})
+            blocks = int(re.match(r'\d+', solution_result[0].get_text())[0])
+            solutions = int(re.match(r'\d+', solution_result[1].get_text())[0])
+
             credit_result = address_row[1].findAll('span', {'class': 'text-aleo-green'})
             try:
                 incentives = float(re.match(r'\d+.\d+', credit_result[0].get_text())[0])
@@ -48,8 +56,12 @@ while True:
             except:
                 coinbase = float(re.match(r'\d+', credit_result[1].get_text())[0])
 
-            print(f'[{dt}] [{address[0]}]-[{address[1][-4:]}] | incentives: {incentives}, coinbase: {coinbase}.')
-            if coinbase >= COINBASE_MIN and incentives >= INCENTIVES_MIN:
-                send_tg_msg(f'[{address[0]}]-[{address[1][-4:]}] | incentives: {incentives}, coinbase: {coinbase}.')
+            message = f'[{dt}] [{address[0]}]-[{address[1][-4:]}] | ' \
+                      f'coinbase: {coinbase}, ' \
+                      f'blocks: {blocks}, ' \
+                      f'solutions: {solutions}.'
+            print(message)
+            if coinbase > COINBASE_MIN and incentives > INCENTIVES_MIN and solutions > SOLUTIONS_MIN:
+                send_tg_msg(message)
 
     time.sleep(SLEEP_TIME_SEC)

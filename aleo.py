9cd07b5c9568fb1b4bad73ec3dfadac93e19013d
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2022-12-08 10:20:10 +0400

    major update

--- aleo.py
+++ aleo.py
@@ -17,7 +17,6 @@ ADDRESSES = {
     "num2-serv2": "aleo...address2",
 }
 
-
 def send_tg_msg(value: str):
     try:
         requests.post(
@@ -31,7 +30,30 @@ def send_tg_msg(value: str):
         print(e.args)
 
 
-while True:
+def get_match(soup: str):
+    try:
+        return float(re.match(r'\d+.\d+', soup)[0])
+    except:
+        return float(re.match(r'\d+', soup)[0])
+
+
+def get_row_items(row: bs4.element.ResultSet):
+    return get_match(soup=row[0].get_text()), get_match(soup=row[0].get_text())
+
+
+def get_address_result(page: requests.Response):
+    soup = BeautifulSoup(page.content, 'html.parser')
+    address_table = soup.findAll('div', {'class': 'address-second-table'})
+
+    solution_row = address_table[1].findAll('span', {'class': 'light-blue-number'})
+    blocks, solutions = get_row_items(row=solution_row)
+
+    credit_row = address_table[1].findAll('span', {'class': 'text-aleo-green'})
+    incentives, coinbase = get_row_items(row=credit_row)
+    return blocks, solutions, incentives, coinbase
+
+
+def work():
     session = requests.session()
     for address in ADDRESSES.items():
         dt = datetime.now().strftime("%H:%M:%S")
@@ -39,28 +61,13 @@ while True:
         if "No matching results" in page.text:
             print(f'[{dt}] [{address[0]}] - [{address[1][-4:]}] | nothing...')
         else:
-            soup = BeautifulSoup(page.content, 'html.parser')
-            address_row = soup.findAll('div', {'class': 'address-second-table'})
-
-            solution_result = address_row[1].findAll('span', {'class': 'light-blue-number'})
-            blocks = int(re.match(r'\d+', solution_result[0].get_text())[0])
-            solutions = int(re.match(r'\d+', solution_result[1].get_text())[0])
-
-            credit_result = address_row[1].findAll('span', {'class': 'text-aleo-green'})
-            try:
-                incentives = float(re.match(r'\d+.\d+', credit_result[0].get_text())[0])
-            except:
-                incentives = float(re.match(r'\d+', credit_result[0].get_text())[0])
-            try:
-                coinbase = float(re.match(r'\d+.\d+', credit_result[1].get_text())[0])
-            except:
-                coinbase = float(re.match(r'\d+', credit_result[1].get_text())[0])
-
+            blocks, solutions, incentives, coinbase = get_address_result(page=page)
             message = f'[{dt}] [{address[0]}]-[{address[1][-4:]}] | ' \
                       f'incentives: {round(incentives, 2)}, ' \
                       f'coinbase: {round(coinbase, 2)}, ' \
                       f'blocks: {blocks}, ' \
                       f'solutions: {solutions}.'
+
             if coinbase > COINBASE_MIN and incentives > INCENTIVES_MIN and solutions > SOLUTIONS_MIN:
                 print(message + 'DONE!')
                 send_tg_msg(value=message)
@@ -68,3 +75,9 @@ while True:
                 print(message)
 
     time.sleep(SLEEP_TIME_SEC)
+
+
+if __name__ == '__main__':
+    while True:
+        work()
+
